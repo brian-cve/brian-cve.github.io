8ebce3005099021f1614f6dfae09777dad6d6d8c
brian <brian.r0397@gmai.com>
2023-02-02 15:37:33 -0500

    Commit QUINTO

--- assets/main.js
+++ assets/main.js
@@ -26,10 +26,12 @@ if(navClose){
 /*========== Remove Menu Mobile ============*/
 
 const navLink = document.querySelectorAll('.nav_link')
-
+/*========== Query selector all selecciona todos los elementos con la clase especificada que en este caso es Navlink ============*/
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
+    // Classlist le da acceso a las clases que tenemos en nuestro css a javascript y despues ejecuta el remove
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+    // ForEach permite que se ejecute la función cada vez por cada elemento en este caso por cada elemento que tenga nav link
