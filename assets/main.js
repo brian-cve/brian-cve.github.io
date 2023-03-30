f9d40e39d7aad18126a4f7aa598b29a23bf01159
brian <brian.r0397@gmai.com>
2023-03-30 18:10:09 -0500

    Añadir sección de trabajo

--- assets/main.js
+++ assets/main.js
@@ -35,3 +35,46 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
     // ForEach permite que se ejecute la función cada vez por cada elemento en este caso por cada elemento que tenga nav link
+
+/*========== Accordion Skills ============*/
+
+const skillsContent = document.getElementsByClassName('skills_content')
+        skillsHeader = document.querySelectorAll(' .skills_header')
+
+function toggleSkills(){
+    let itemclass = this.parentNode.className;
+      // this.parentNode, me permite sacar el elemento del documento global
+        for (i = 0; i < skillsContent.length; i++) {
+          skillsContent[i].className = 'skills_content skills_close'
+            }
+        if (itemclass === 'skills_content skills_close') {
+            this.parentNode.className = 'skills_content skills_open'
+        }
+   }    
+
+skillsHeader.forEach((al) => {
+    al.addEventListener('click', toggleSkills)
+})
+
+/*========== Qualification Tabs ============*/
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents= document.querySelectorAll('[data-content]')
+
+    tabs.forEach(tab =>{
+        tab.addEventListener('click', ()=>{
+            const target = document.querySelector(tab.dataset.target)
+            
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('qualification_active')
+            })
+
+            target.classList.add ('qualification_active')
+
+            tabs.forEach(tab => {
+                tab.classList.remove('qualification_active')
+            })
+            tab.classList.add('qualification_active')
+        })
+    })
+
